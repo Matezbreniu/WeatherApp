e5e045eac2a33d47e310e59c42da26811a6dd556
Mateusz Bręk <matezbreniu@gmail.com>
2020-06-07 15:48:14 +0200

    Updates

---
+++ precache-manifest.73ee596baaf64fe3535f15b61b751aab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c186a0014ab28b7893d45bf8ea64345c",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "f8d3f806a8f5ba26af01",
+    "url": "/weatherapp/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "e54893d0731e7897f3bc",
+    "url": "/weatherapp/static/css/main.89423acb.chunk.css"
+  },
+  {
+    "revision": "f8d3f806a8f5ba26af01",
+    "url": "/weatherapp/static/js/2.485a0d23.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weatherapp/static/js/2.485a0d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54893d0731e7897f3bc",
+    "url": "/weatherapp/static/js/main.b61c03cd.chunk.js"
+  },
+  {
+    "revision": "26373162a9e6f2118022",
+    "url": "/weatherapp/static/js/runtime-main.d5bba109.js"
+  }
+]);
